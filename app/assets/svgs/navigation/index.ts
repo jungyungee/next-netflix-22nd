44843b0591a66d7b78684124391ed966072ad69b
fkgrkyr <fkgrkyr@gmail.com>
2025-10-31 18:12:24 +0900

    #5 [FEAT] 하단 네브바 구현

---
+++ app/assets/svgs/navigation/index.ts
@@ -0,0 +1,5 @@
+export { default as ComingSoonIcon } from './ComingSoonIcon';
+export { default as DownloadIcon } from './DownloadIcon';
+export { default as HomeIcon } from './HomeIcon';
+export { default as MoreIcon } from './MoreIcon';
+export { default as SearchIcon } from './SearchIcon';
