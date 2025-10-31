b722fbed0a94da4fcecbde4f12b8741c2ddba6eb
fkgrkyr <fkgrkyr@gmail.com>
2025-10-31 18:33:29 +0900

    #5 [FEAT] 헤더바 구현

--- app/assets/svgs/navigation/index.ts
+++ app/assets/svgs/navigation/index.ts
@@ -2,4 +2,5 @@ export { default as ComingSoonIcon } from './ComingSoonIcon';
 export { default as DownloadIcon } from './DownloadIcon';
 export { default as HomeIcon } from './HomeIcon';
 export { default as MoreIcon } from './MoreIcon';
+export { default as NetflixIcon } from './NetflixIcon';
 export { default as SearchIcon } from './SearchIcon';
